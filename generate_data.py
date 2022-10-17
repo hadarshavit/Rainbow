3133e4ad1056adb49a64a106f58cc7834292d924
Hadar Shavit <shavit.hadar@gmail.com>
2022-10-17 22:40:32 +0000

    Logs for gen data

--- generate_data.py
+++ generate_data.py
@@ -1,12 +1,14 @@
 from common.env_wrappers import create_env_instance
 from argparse import Namespace
 import numpy as np
+import tqdm
 
 envs = ['Alien', 'Amidar', 'Assault', 'Asterix', 'Asteroids', 'Atlantis', 'BankHeist', 'BattleZone', 'BeamRider', 'Berzerk', 'Bowling', 'Boxing', 'Breakout', 'Centipede', 'ChopperCommand', 'CrazyClimber', 'Defender', 'DemonAttack', 'DoubleDunk', 'Enduro', 'FishingDerby', 'Freeway', 'Frostbite', 'Gopher', 'Gravitar', 'Hero', 'IceHockey', 'Kangaroo', 'Krull', 'KungFuMaster', 'MontezumaRevenge', 'MsPacman', 'NameThisGame', 'Phoenix', 'Pitfall', 'Pong', 'PrivateEye', 'Qbert', 'RoadRunner', 'Robotank', 'Seaquest', 'Skiing', 'Solaris', 'SpaceInvaders', 'StarGunner', 'Tennis', 'TimePilot', 'Tutankham', 'Venture', 'VideoPinball', 'WizardOfWor', 'YarsRevenge', 'Zaxxon']
 
 if __name__ == '__main__':
     for env in envs:
-        for i in range(50_000):
+        print(env)
+        for i in tqdm(range(50_000)):
             args = Namespace(env_name=f'gym:{env}', seed=1, time_limit=100_000, frame_skip=4, gamma=0.99, resolution=(224, 224), grayscale=True)
             env = create_env_instance(args, instance=i, decorr_steps=0)
             done = False
